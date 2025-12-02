d896e9f5acba685e4ea0e2194aac8a07d3404ece
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-12-02 21:16:32 +0530

    changes

--- client.js
+++ client.js
@@ -1,10 +1,22 @@
 const net = require("net");
+const readline = require("readline/promises");
 
-const client = net.createConnection({ host: "127.0.0.1", port: 3008 }, () => {
+const rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+})
+
+const socket = net.createConnection({ host: "127.0.0.1", port: 3008 }, async() => {
   console.log("Connected to the server!");
+
+  const message = await rl.question("Enter a message >")
+  socket.write(message)
 });
 
+socket.on("data", (data)=>{
+  console.log(data.toString("utf-8"))
+})
 
-client.on("end", () => {
+socket.on("end", () => {
   console.log("Connection was ended!!");
 });
