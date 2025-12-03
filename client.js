453df5fd51d37fda3c3484b4a176e139c5e4868c
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-12-04 02:56:11 +0530

    implement 'moveCursor' and 'clearLine' functions

--- client.js
+++ client.js
@@ -6,16 +6,44 @@ const rl = readline.createInterface({
   output:process.stdout
 })
 
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
+
 const socket = net.createConnection({ host: "127.0.0.1", port: 3008 }, async() => {
   console.log("Connected to the server!");
 
-  const message = await rl.question("Enter a message >")
-  socket.write(message)
-});
+  const ask = async () => {
+      const message = await rl.question("Enter a message > ");
+      await moveCursor(0, -1);
+      await clearLine(0);
+      socket.write(message);
+    };
 
-socket.on("data", (data)=>{
-  console.log(data.toString("utf-8"))
-})
+    ask();
+
+    socket.on("data", async (data) => {
+      console.log()
+      await moveCursor(0,-1)
+      await clearLine(0);
+      console.log(data.toString("utf-8"));
+      ask()
+    });
+
+});
 
 socket.on("end", () => {
   console.log("Connection was ended!!");
